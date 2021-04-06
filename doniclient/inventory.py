d69f4c1e3280447e44147e93b9034252220eff20
Michael Sherman <shermanm@uchicago.edu>
2021-04-05 22:01:26 -0400

    use command.Lister for standard output formatting

--- doniclient/inventory.py
+++ doniclient/inventory.py
@@ -13,7 +13,7 @@ LOG = logging.getLogger(__name__)  # Get the logger of this module
 SERVICE_TYPE = "inventory"
 
 
-class InventoryApi(command.Command):
+class InventoryList(command.Lister):
     """Base class for Inventory API."""
 
     def _get_api(self):
@@ -25,33 +25,63 @@ class InventoryApi(command.Command):
         return hw_api
 
 
-class List(InventoryApi):
+class ListHardware(InventoryList):
     """List all hw items in Doni."""
 
     def take_action(self, parsed_args):
         """List all hw items in Doni."""
         hw_api = self._get_api()
-        response = hw_api.list("/v1/hardware/")
-        return response
+        data = hw_api.list("/v1/hardware/")
+        columns = (
+            "name",
+            "project_id",
+            "hardware_type",
+            "properties",
+        )
+
+        data_iterator = (
+            utils.get_dict_properties(s, columns, formatters={})
+            for s in data.get("hardware")
+        )
+        return (columns, data_iterator)
 
 
-class Export(InventoryApi):
+class ExportHardware(InventoryList):
     """List all hw items in Doni."""
 
     def take_action(self, parsed_args):
         """List all hw items in Doni."""
         hw_api = self._get_api()
-        response = hw_api.list("/v1/hardware/export/")
-        return response
+        data = hw_api.list("/v1/hardware/export/")
+        print(data)
+        columns = (
+            "name",
+            "uuid",
+            "project_id",
+            "hardware_type",
+            "properties",
+        )
+
+        data_iterator = (
+            utils.get_dict_properties(s, columns, formatters={})
+            for s in data.get("hardware")
+        )
+        return (columns, data_iterator)
 
 
-class Get(InventoryApi):
+class GetHardware(command.ShowOne):
     """List all hw items in Doni."""
+
+    def get_parser(self, prog_name):
+        """Add arguments to cli parser."""
+        parser = super(Get, self).get_parser(prog_name)
+        parser.add_argument("--uuid", metavar="<hw_uuid>", help=("UUID of hw item"))
+        return parser
 
     def take_action(self, parsed_args):
         """List all hw items in Doni."""
         hw_api = self._get_api()
-        response = hw_api.get("/v1/hardware/")
+        response = hw_api.find(f"/v1/hardware/{parsed_args.hw_uuid}")
         return response
 
 
