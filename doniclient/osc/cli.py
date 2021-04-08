4f27026649bacf6bac96eb86952007e52f802849
Michael Sherman <shermanm@uchicago.edu>
2021-04-08 13:45:55 -0400

    create hardware works

--- doniclient/osc/cli.py
+++ doniclient/osc/cli.py
@@ -2,6 +2,7 @@
 
 import logging
 
+from keystoneauth1.exceptions import BadRequest, Conflict
 from osc_lib import utils
 from osc_lib.cli import parseractions
 from osc_lib.command import command
@@ -84,44 +85,78 @@ class GetHardware(command.ShowOne):
 class CreateHardware(command.Command):
     """Create a Hardware Object in Doni."""
 
+    optional_args = [
+        "ipmi_username",
+        "ipmi_password",
+        "ipmi_terminal_port",
+    ]
+
     def get_parser(self, prog_name):
         """Add arguments to cli parser."""
-        parser = super(GetHardware, self).get_parser(prog_name)
+        parser = super(CreateHardware, self).get_parser(prog_name)
         parser.add_argument(
-            "--name", metavar="<name>", help=("human readable name of hw item")
+            "--name",
+            metavar="<name>",
+            help=("human readable name of hw item"),
+            required=True,
         )
         parser.add_argument(
             "--hardware_type",
             metavar="<hardware_type>",
             default="baremetal",
             help=("hardware_type of item"),
+            required=True,
         )
+
+        parser.add_argument("--mgmt_addr", metavar="<mgmt_addr>", required=True)
+        parser.add_argument("--ipmi_username", metavar="<ipmi_username>")
+        parser.add_argument("--ipmi_password", metavar="<ipmi_password>")
         parser.add_argument(
-            "--properties",
-            metavar="<properties>",
-            default={},
-            help=("hardware_type of item"),
+            "--ipmi_terminal_port", metavar="<ipmi_terminal_port>", type=int
+        )
+
+        parser.add_argument(
+            "--interface",
+            required_keys=["name", "mac"],
+            action=parseractions.MultiKeyValueAction,
+            help=(
+                "Specify once per interface, in the form:\n `--interface name=<name>,mac=<mac_address>`"
+            ),
         )
         return parser
 
     def take_action(self, parsed_args):
         """List all hw items in Doni."""
-        body = {
+        json_body = {
             "name": parsed_args.name,
-            "hardware_type": parsed_args.hw_type,
-            "properties": {},
+            "hardware_type": parsed_args.hardware_type,
+            "properties": {
+                "management_address": parsed_args.mgmt_addr,
+                "interfaces": [],
+            },
         }
+
+        for interface_dict in parsed_args.interface:
+            name = interface_dict.get("name")
+            mac_addr = interface_dict.get("mac")
+
+            json_body["properties"]["interfaces"].append(
+                {
+                    "name": name,
+                    "mac_address": mac_addr,
+                }
+            )
+
+        for arg in self.optional_args:
+            json_body["properties"][arg] = getattr(parsed_args, arg)
 
         hw_client = self.app.client_manager.inventory
         try:
-            data = hw_client.create(body)
-        except Exception as ex:
-            raise ex
-
-        return (
-            self.columns,
-            utils.get_dict_properties(data, self.columns, formatters={}),
-        )
+            data = hw_client.create(json_body)
+        except (BadRequest, Conflict) as ex:
+            print(f"got error {ex.response}: {ex.response.text}")
+        else:
+            return data
 
 
 class UpdateHardware(command.Command):
